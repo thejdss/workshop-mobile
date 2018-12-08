278ea25073a1758fc398c01222578207dddec4c7
Jaelcio <jaelcio99@gmail.com>
2018-12-07 22:42:26 -0200

    Workshop finished

---
+++ Assets/Workshop/Scripts/Bird.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Bird : MonoBehaviour {
+
+    public float flyForce;
+    public Rigidbody componentRigidbody;
+    public int score;
+    public Text scoreText;
+    public GameObject canvas;
+
+    void Start()
+    {
+        componentRigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            componentRigidbody.velocity = Vector3.zero;
+            componentRigidbody.AddForce(Vector3.up * flyForce, ForceMode.Impulse);
+        }
+    }
+
+    void LateUpdate()
+    {
+        float speed = componentRigidbody.velocity.y;
+        this.transform.rotation = Quaternion.Euler(0,180f, speed * -7);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Kill"))
+        {
+            canvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (other.CompareTag("Score"))
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
+    }
+}
---
+++ Assets/Workshop/Scripts/ButtonFunctions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void GameTime(int time)
+    {
+        Time.timeScale = time;
+    }
+}
